14aaaaae48896cb4c143491ffe83eea95adc5ae5
Ariana Ledezma <aeledezma72@gmail.com>
2023-10-12 12:23:32 -0700

    Linked GitHub repository in footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        Project by{" "}
+        <a
+          href="https://zippy-dango-d04e9d.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ariana Ledezma
+        </a>{" "}
+        is{" "}
+        <a
+          href="https://github.com/ariaLed72/react-weather-app-2.0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
